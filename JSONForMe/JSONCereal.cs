760096fe4ed96f706407f5752747294169a813e9
ryan monaghan <rmonaghan90@gmail.com>
2018-03-04 18:51:56 -0800

    3.4.2018

--- JSONForMe/JSONCereal.cs
+++ JSONForMe/JSONCereal.cs
@@ -9,18 +9,39 @@ namespace JSONForMe
 {
     public static class JSONCereal
     {
+        public static StringBuilder bobTheBuilder = new StringBuilder();
 
-
-        public static string Cerial(object obj)
+        //gets the properties of an object into a string list
+        public static List<string> GetPropNames(object obj)
         {
-            var type = obj.GetType();
-            var things = type.GetProperties();
-            StringBuilder bobTheBuilder = new StringBuilder();
-            foreach(PropertyInfo info in things)
+            List<string> tempStrList = new List<string>(obj.GetType().GetProperties().Count());
+            foreach (var thing in obj.GetType().GetProperties())
             {
-                bobTheBuilder.Append(info);
+                tempStrList.Add(thing.Name);
+            }
+            return tempStrList;
+        }
+
+        //gets strings from sting list
+        public static string GetNameFromList(List<string> strList)
+        {
+            for(int i =0; i < strList.Count; i++)
+            {
+                 bobTheBuilder.Append(GetPropName(strList, i));
             }
             return bobTheBuilder.ToString();
         }
+
+        //gets individual names from list
+        public static string GetPropName(List<string> strList, int numb)
+        {
+            return strList[numb];
+        }
+        
+        // gets value from property
+        public static string GetPropertyValue(object obj, string property)
+        {
+            return obj.GetType().GetProperty(property).GetValue(obj, null).ToString();
+        }
     }
-} 
+}
--- JSONForMe/Program.cs
+++ JSONForMe/Program.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
-namespace JSONForMe
-{
+ namespace JSONForMe
+ {
     class Program
     {
         static void Main(string[] args)
         {
-            Dog Pupper = new Dog() { Name = "Woofer" };
-            Console.WriteLine(JSONCereal.Cerial(Pupper));
+            Dog Pupper = new Dog() { Name = "Woofer", Legs = 4 };
+            foreach (string thing in JSONCereal.GetPropNames(Pupper))
+            {
+                int i = 0;
+                Console.WriteLine(thing);
+                Console.WriteLine(JSONCereal.GetPropertyValue(Pupper, JSONCereal.GetPropName(JSONCereal.GetPropNames(Pupper), i)));
+                i++;
+            }
+            //Console.WriteLine(JSONCereal.GetPropertyValue(Pupper, JSONCereal.GetPropName(JSONCereal.GetPropNames(Pupper), 0)));
+            //Console.WriteLine(JSONCereal.GetPropertyValue(Pupper, JSONCereal.GetNameFromList
+            //                 (JSONCereal.GetPropNames())));
             Console.ReadKey();
         }
     }
-}
+ }
